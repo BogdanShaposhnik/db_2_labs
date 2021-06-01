7124bee82c268a3a9b9d78d16ba3ff04c3dd1258
Bogdan Shaposhnik <bogdanshaposhnic@gmail.com>
2021-06-01 12:08:01 +0300

    Update task1.py

--- db_lab1/task1.py
+++ db_lab1/task1.py
@@ -6,7 +6,10 @@ root = None
 print('Average count of text fragments per p')
 with open('results/kpi.xml', 'r') as file:
     root = etree.parse(file)
-
+#root = tree.getroot()
+#urls = root.findall("page")
+#for url in urls:
+    #print(url.get('url'))
 
 
 
